57d0f2b9452fbd1fbac54cdfda79c53b9fc8a3f3
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2025-04-14 09:54:50 +0530

    Address review comments

--- json-rules-core/src/main/java/io/appform/jsonrules/jsonpath/providers/OptimizedJacksonJsonNodeJsonProvider.java
+++ json-rules-core/src/main/java/io/appform/jsonrules/jsonpath/providers/OptimizedJacksonJsonNodeJsonProvider.java
@@ -2,34 +2,19 @@ package io.appform.jsonrules.jsonpath.providers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.common.annotations.VisibleForTesting;
 import com.jayway.jsonpath.Option;
 import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import lombok.Getter;
 
 import java.util.Set;
 
 public class OptimizedJacksonJsonNodeJsonProvider extends JacksonJsonNodeJsonProvider {
+    @Getter
     private boolean shouldReturnPathAsList;
 
     public OptimizedJacksonJsonNodeJsonProvider(Set<Option> options) {
         this.shouldReturnPathAsList = options.contains(Option.AS_PATH_LIST);
-    }
-
-    private ArrayNode toJsonArray(Object o) {
-        return (ArrayNode) o;
-    }
-
-    private JsonNode createJsonElement(Object o) {
-        if (o != null) {
-            // jlolling: avoid creating a cloned node: bug #211
-            if (o instanceof JsonNode) {
-                return (JsonNode) o;
-            } else {
-                // [PATCHED]
-                return shouldReturnPathAsList ? objectMapper.valueToTree(o) : null;
-            }
-        } else {
-            return null;
-        }
     }
 
     @Override
@@ -46,4 +31,23 @@ public class OptimizedJacksonJsonNodeJsonProvider extends JacksonJsonNodeJsonPro
         }
     }
 
+    @VisibleForTesting
+    JsonNode createJsonElement(Object o) {
+        if (o != null) {
+            // jlolling: avoid creating a cloned node: bug #211
+            if (o instanceof JsonNode) {
+                return (JsonNode) o;
+            } else {
+                // [PATCHED]
+                // Convert the object to a JsonNode only if Option.AS_PATH_LIST is set
+                return shouldReturnPathAsList ? objectMapper.valueToTree(o) : null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private ArrayNode toJsonArray(Object o) {
+        return (ArrayNode) o;
+    }
 }
